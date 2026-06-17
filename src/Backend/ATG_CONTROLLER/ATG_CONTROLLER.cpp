1a5efe9980c8fe84c45cd077f1b262df718f8ad7
Gehrke <alexander_gehrke@brown.edu>
2026-06-17 19:32:37 -0400

    different phase control implementations

--- src/Backend/ATG_CONTROLLER/ATG_CONTROLLER.cpp
+++ src/Backend/ATG_CONTROLLER/ATG_CONTROLLER.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33a7393ef47f674d976ccf1028942d0fa63dea07344af42d409693b963a2320f
-size 139630
+oid sha256:d1e3f858033ed8cfd0a32f8597b881295b8c0579ce4a0467eac6c75e26a0ea1a
+size 230466
